8f4756de07bf34d30ed169adcc7c31606565945a
Vinicius Gomes <viniciusgomes05@hotmail.com>
2022-10-31 22:32:56 -0300

    Resolução de alguns exercicios

---
+++ tipos-variaveis/src/MetodosDio/Ex1Met.java
@@ -0,0 +1,13 @@
+package MetodosDio.java;
+
+/*
+ * Criar uma aplicação que resolva as situações :
+ * 1° calcule as 4 operações. sempre 2 valores devem ser passados.
+ * 2° partir da hora do dia,informe a mensagem adequada.
+ * 3°calcule o valor final de um empréstimo, a partir do valor solicitado. taxas e parcelam influenciam
+ * Defina arbitrariamente as faixas que influenciam nos valores
+ */
+
+public class Ex1Met {
+    
+}
